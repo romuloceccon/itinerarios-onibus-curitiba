5cf6f748280e2be63235e5b144b6bc6e38460def
Romulo A. Ceccon <romuloceccon@gmail.com>
2012-10-23 09:17:36 -0200

    ajustes; readme

--- importar_itinerarios.rb
+++ importar_itinerarios.rb
@@ -1,7 +1,6 @@
 require 'net/http'
 require 'uri'
 require 'nokogiri'
-require 'iconv'
 require 'yaml'
 
 uri = URI("http://urbs-web.curitiba.pr.gov.br/centro/conteudo_lista_linhas.asp?l='n'")
@@ -9,6 +8,8 @@ uri = URI("http://urbs-web.curitiba.pr.gov.br/centro/conteudo_lista_linhas.asp?l
 response = Net::HTTP.start(uri.host, uri.port) do |http|
   http.get(uri.request_uri).body
 end
+
+Dir.mkdir('itinerarios') unless File.exist?('itinerarios')
 
 html = Nokogiri::HTML(response)
 select = html.xpath('/html/body/table/tr/td/p/select')
@@ -36,12 +37,11 @@ select.xpath('option').each do |option|
     if (title = td.xpath('strong')).empty? then
       data[index] << td.text
     else
-      index = title.text.match(/\b.*\b/).to_s
+      index = title.text.match(/\w.*\w/).to_s
     end
   end
 
-  Dir.mkdir('itinerarios')
-  File.open("result/#{t.gsub('/', '-')}.yml", 'w+') do |x|
-    x << { 'CODIGO' => v }.merge(data).to_yaml
+  File.open("itinerarios/#{t.gsub('/', '-')} (#{v}).yml", 'w') do |x|
+    x << { 'CODIGO' => v, 'NOME' => t }.merge(data).to_yaml
   end
 end
