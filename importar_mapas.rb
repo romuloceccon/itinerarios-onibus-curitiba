e7075e92b32ac0da60f34d1651ef54f9a05a0e8b
Romulo A. Ceccon <romuloceccon@gmail.com>
2012-10-24 18:30:05 -0200

    correções no download dos tiles

--- importar_mapas.rb
+++ importar_mapas.rb
@@ -7,9 +7,9 @@ TILE_HEIGHT = 500.0 # 1250.0, 500.0, 200.0
 DATABASE = 'map_download.yml'
 
 def generate_tile(linha, req_id, options = {})
-  x0 = options[:x0].to_f
-  y0 = options[:y0].to_f
-  h = options[:h] || 500.0
+  x0 = options['x0'].to_f
+  y0 = options['y0'].to_f
+  h = options['h'] || 500.0
   
   if x0 == 0.0
     cx = '0.0'
@@ -65,50 +65,59 @@ def save_database
   File.open(DATABASE, 'w+') { |x| YAML.dump($database, x) }
 end
 
-unless File.exist?(DATABASE)
-  $database = { }
-  get_linhas_curitiba.each_pair do |k, v|
-    $database[k] = { 'nome' => v }
+if __FILE__ == $0
+  unless File.exist?(DATABASE)
+    $database = { }
+    get_linhas_curitiba.each_pair do |k, v|
+      $database[k] = { 'nome' => v }
+    end
+    save_database
+  else
+    $database = File.open(DATABASE) { |x| YAML.load(x) }
   end
-  save_database
-else
-  $database = File.open(DATABASE) { |x| YAML.load(x) }
-end
-
-$database.each_pair do |k, v|
-  next if v['skip']
-  next if v['map']
   
-  req_id = rand(1_000_000)
-  x0, y0, xn, yn = generate_tile(k, req_id)
-  tiles = get_tile_coordinates(x0, y0, xn, yn)
-  file_name = "map_#{k}.gif"
-  STDERR.puts "Downloading map #{k} (#{v['nome']}) to #{file_name}"
-  download_tile(file_name, req_id)
-  
-  $database[k]['map'] = { 'x0' => x0, 'y0' => y0, 'xn' => xn, 'yn' => yn, 'file_name' => file_name, 'tiles' => tiles }
-  save_database
-end
-
-$database.each_pair do |k, v|
-  map = v['map']
-  tiles = map && v['map']['tiles']
-  next unless tiles
-  
-  tiles.each do |tile|
-    next if tile['file_name']
+  $database.each_pair do |k, v|
+    next if v['skip']
+    next if v['map']
     
-    row, col = tile['row'], tile['col']
+    file_name = "#{k}.gif"
+    STDERR.puts "Downloading map #{k} (#{v['nome']}) to #{file_name}"
 
     req_id = rand(1_000_000)
-    generate_tile(k, req_id, tile['request_options'])
+    x0, y0, xn, yn = generate_tile(k, req_id)
+    tiles = get_tile_coordinates(x0, y0, xn, yn)
     
-    Dir.mkdir(k) unless File.exist?(k)
-    file_name = "k/%02d_%02d.gif" % [row, col]
-    STDERR.puts "Downloading tile #{row},#{col} to #{file_name}"
     download_tile(file_name, req_id)
     
-    tile['file_name'] = file_name
+    tile_database = "#{k}.yml"
+    File.open(tile_database, 'w+') { |x| YAML.dump(tiles, x) }
+    
+    $database[k]['map'] = { 'x0' => x0, 'y0' => y0, 'xn' => xn, 'yn' => yn,
+        'file_name' => file_name, 'database' => tile_database }
     save_database
+  end
+  
+  $database.each_pair do |k, v|
+    map = v['map']
+    tile_database = map && v['map']['database']
+    next unless tile_database
+    
+    tiles = File.open(tile_database) { |x| YAML.load(x) }
+    tiles.each do |tile|
+      next if tile['file_name']
+      
+      row, col = tile['row'], tile['col']
+      file_name = "#{k}/%02d_%02d.gif" % [row, col]
+      STDERR.puts "Downloading tile #{row},#{col} to #{file_name}"
+
+      req_id = rand(1_000_000)
+      generate_tile(k, req_id, tile['request_options'])
+      
+      Dir.mkdir(k) unless File.exist?(k)
+      download_tile(file_name, req_id)
+      
+      tile['file_name'] = file_name
+      File.open(tile_database, 'w+') { |x| YAML.dump(tiles, x) }
+    end
   end
 end
